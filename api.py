eaa7f9f0014ebcc80638c6ec7383bb470201449d
fcubric <f.cubric@gmail.com>
2023-06-03 12:56:35 +0200

    Initial commit

---
+++ api.py
@@ -0,0 +1,23 @@
+# API for zp proj
+
+def generate_new_keypair():
+    pass
+
+
+def delete_keypair():
+    pass
+
+def import_key():
+    pass
+
+def export_key():
+    pass
+
+def show_ring():
+    pass
+
+def send_message(msg):
+    pass
+
+def receive_message():
+    pass
