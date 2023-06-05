fcbb51699c8134dbf8bd2fd7644c7b733d619923
za200009d <za200009d@student.etf.bg.ac.rs>
2023-06-06 01:01:03 +0200

    Add docstrings in api.py and message field in 'send message' tab

--- api.py
+++ api.py
@@ -8,30 +8,82 @@ def generate_new_keypair(name, password, email, size, algorithm):
     :param email: email of user
     :param size: chosen keysize
     :param algorithm: chosen algorithm (possible values: 'RSA', 'DSA')
-    :return: message saying the operation was successful or error message
+    :return:  descriptive message / error
     '''
     return ""
-
 
 def delete_keypair(keys):
     '''
 
-    :param keys: not sure what this is yet
-    :return:
+    :param keys: id of key pair in the structure to be deleted
+    :return: descriptive message / error
     '''
     return ""
 
 def import_key(filename, path, password, req):
+    '''
+
+    :param filename: file from which key is imported
+    :param path: path to file
+    :param password: user password
+    :param req: if the password is required (True-private key, False-public key)
+    :return: descriptive message / error
+    '''
     return ""
 
 def export_key(filename, path, password, req):
+    '''
+
+    :param filename: file to which key is exported
+    :param path: path to file
+    :param password: user password
+    :param req: if the password is required (True-private key, False-public key)
+    :return: descriptive message / error
+    '''
     return ""
 
 def show_ring(pw):
+    '''
+
+    :param pw: password for showing a private key pair
+    :return: descriptive message / error
+    '''
     return ""
 
-def send_message(filename, path, enc, sign, compress, radix):
+def send_message(filename, path, enc, sign, compress, radix, message):
+    '''
+
+    :param filename: file to which message is exported
+    :param path: path to file
+    :param enc: encription object
+            enc={
+                'alg' : algorithm of encryption
+                'key' : encryption public key
+            }
+            can be empty if the option for encryption is not chosen
+    :param sign: signature object
+            sign={
+                'alg' : algorithm of signature
+                'key' : signature private key
+            }
+            can be empty if the option for signature is not chosen
+    :param compress: True if compress option is chosen, otherwise False
+    :param radix: True if conversion option is chosen, otherwise False
+    :param message: message to be seng
+    :return: descriptive message / error
+    '''
     return ""
 
 def receive_message(filename_from,path_from,filename_to,path_to,errors):
+    '''
+
+    :param filename_from: file from which message is read
+    :param path_from: path to file_form
+    :param filename_to: file to which decrypted message is stored
+    :param path_to: path to file_to
+    :param errors: array of length 2
+            errors[0] - says whether the authentication was successful
+            errors[1] - says whether the verificatoin was successful
+    :return: descriptive message / error
+    '''
     return ""
