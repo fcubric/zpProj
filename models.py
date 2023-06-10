de6515a3af12dce16ae9b7ffd90749e693ef31fe
fcubric <f.cubric@gmail.com>
2023-06-10 15:12:16 +0200

    Add rsa key generation

--- models.py
+++ models.py
@@ -1,5 +1,11 @@
+import hashlib
 from datetime import datetime
+
+from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+import os
+
 
 class KeyRingPrivate:
     def __init__(self,email,name, password,key_size,algorithm):
@@ -9,12 +15,33 @@ class KeyRingPrivate:
         self.public_key=None
         self.private_key=None
         self.timestamp=datetime.now()
+        cast_key = hashlib.sha1(password.encode()).digest()
+        initial_vector = os.urandom(16)
+        cipher = Cipher(algorithms.CAST5(cast_key), mode=modes.CBC(initial_vector))
+        encryptor = cipher.encryptor()
+
+
+        print(cast_key)
         if algorithm=="RSA":
-            self.private_key = rsa.generate_private_key(
+            real_prviateKey = rsa.generate_private_key(
                 public_exponent=65537,
                 key_size=key_size
             )
-            self.public_key = self.private_key.public_key()
+            private_key_bytes = real_prviateKey.private_bytes(
+                encoding=serialization.Encoding.PEM,
+                format=serialization.PrivateFormat.PKCS8,
+                encryption_algorithm=serialization.NoEncryption()
+            )
+            print("PRIVATNI KLJUC", real_prviateKey , "\nPRIVATNI KLJUC BYTES", private_key_bytes)
+
+            self.private_key = encryptor.update(private_key_bytes) + encryptor.finalize()
+            self.public_key =real_prviateKey.public_key()
+
+            decryptor = cipher.decryptor()
+            private_key_bytes2 = decryptor.update(self.private_key) + decryptor.finalize()
+            print("PRIVATNI KLJUC", real_prviateKey, "\nPRIVATNI KLJUC BYTES", private_key_bytes)
+
+
         else:
             KeyRingPrivate.generate_key_pair_elgamal_dsa(self,key_size)
         self.keyId=self.public_key.public_numbers().n % 2**64
